bbf843a21c774f78d9e4d06cfa4c31bb8031f712
JAMES-BONT-007 <90338385+JAMES-BONT-007@users.noreply.github.com>
2022-05-16 22:48:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,59 @@
 anime({
-    targets:'div.kwadrat',
+    targets:'div.kwadrat.czer',
     translateY:[
     { value: 200, duration: 400 },
-    { value: 300, duration: 500},
-    { value: 100, duration: 700}
+    { value: 300, duration: 600},
+    { value: 100, duration: 700},
+    { value: 100, duration: 1100},
+    { value: 550, duration: 500}
     ],
     rotate:{
         value: "4turn",
+        easing: "easeInOutSine"
+    }
+})
+
+anime({
+    targets:'div.kwadrat.nieb',
+    translateY:[
+    { value: 300, duration: 400 },
+    { value: 350, duration: 650},
+    { value: 125, duration: 750},
+    { value: 125, duration: 1000},
+    { value: 550, duration: 500}
+    ],
+    rotate:{
+        value: "6turn",
+        easing: "easeInOutSine"
+    }
+})
+
+anime({
+    targets:'div.kwadrat.ziel',
+    translateY:[
+    { value: 450, duration: 400 },
+    { value: 400, duration: 600},
+    { value: 155, duration: 400},
+    { value: 155, duration: 1400},
+    { value: 550, duration: 500}
+    ],
+    rotate:{
+        value: "3turn",
+        easing: "easeInOutSine"
+    }
+})
+
+anime({
+    targets:'div.kwadrat.zol',
+    translateY:[
+    { value: -110, duration: 400 },
+    { value: 105, duration: 700},
+    { value: 200, duration: 400},
+    { value: 200, duration: 1300},
+    { value: 550, duration: 500}
+    ],
+    rotate:{
+        value: "5turn",
         easing: "easeInOutSine"
     }
 })
